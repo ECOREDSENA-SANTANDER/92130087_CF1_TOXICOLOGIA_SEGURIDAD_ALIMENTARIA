e280dfe0ddcae471b39d5cc3ee5fa3728f3cd33c
Andrea Botello <andrea.botellor@misena.edu.co>
2024-10-29 11:29:43 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,8 +13,8 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video. Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/3SHKqNDIEQs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video. Antinutrientes y toxicidad alimentaria 
 
 
 </template>
